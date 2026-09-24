d31c5fe955fff663d1c380a1bfad58075f73cb22
Woss23 <154534258+Woss23@users.noreply.github.com>
2026-09-24 09:33:41 -0300

    JS

--- JS/pagina4.js
+++ JS/pagina4.js
@@ -1,27 +1,24 @@
-
-
-// Pega os botões dos cenários
 const botao1 = document.getElementById("header-cenario-1");
 const botao2 = document.getElementById("header-cenario-2");
 const botao3 = document.getElementById("header-cenario-3");
 const botao4 = document.getElementById("header-cenario-4");
 
 
-// Pega o conteúdo dos cenários
+
 const cenario1 = document.getElementById("cenario-1");
 const cenario2 = document.getElementById("cenario-2");
 const cenario3 = document.getElementById("cenario-3");
 const cenario4 = document.getElementById("cenario-4");
 
 
-// Esconde os passos inicialmente
+
 cenario1.querySelector("ol").style.display = "none";
 cenario2.querySelector("ol").style.display = "none";
 cenario3.querySelector("ol").style.display = "none";
 cenario4.querySelector("ol").style.display = "none";
 
 
-// Cenário 1
+
 botao1.addEventListener("click", function() {
 
     const passos = cenario1.querySelector("ol");
@@ -35,7 +32,7 @@ botao1.addEventListener("click", function() {
 });
 
 
-// Cenário 2
+
 botao2.addEventListener("click", function() {
 
     const passos = cenario2.querySelector("ol");
@@ -49,7 +46,7 @@ botao2.addEventListener("click", function() {
 });
 
 
-// Cenário 3
+
 botao3.addEventListener("click", function() {
 
     const passos = cenario3.querySelector("ol");
@@ -63,7 +60,7 @@ botao3.addEventListener("click", function() {
 });
 
 
-// Cenário 4
+
 botao4.addEventListener("click", function() {
 
     const passos = cenario4.querySelector("ol");
